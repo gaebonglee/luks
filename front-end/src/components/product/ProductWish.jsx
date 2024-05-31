5dd01b991e6601406f5776eb6e85a5afa7fb7afd
gayoung lee <gost1073@gmail.com>
2024-06-01 07:58:33 +0900

    비로그인시, 좋아요 부분 업데이트

--- front-end/src/components/product/ProductWish.jsx
+++ front-end/src/components/product/ProductWish.jsx
@@ -2,13 +2,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { FiHeart } from "react-icons/fi";
 import { FaHeart } from "react-icons/fa";
+import Swal from "sweetalert2"; // SweetAlert2 라이브러리 사용
 
 const ProductWish = ({ productId }) => {
   const [liked, setLiked] = useState(false); // 초기값 false
   const [error, setError] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // 로그인 상태
 
   useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/check-session`,
+          {
+            withCredentials: true,
+          }
+        );
+        setIsLoggedIn(response.data.loggedIn);
+      } catch (error) {
+        console.error("There was an error checking the login status!", error);
+      }
+    };
+
     const fetchWishStatus = async () => {
+      if (!isLoggedIn) {
+        return;
+      }
       try {
         const response = await axios.get(
           `http://localhost:5000/wishlist/item-status/${productId}`,
@@ -31,10 +50,27 @@ const ProductWish = ({ productId }) => {
       }
     };
 
+    checkLoginStatus();
     fetchWishStatus();
-  }, [productId]);
+  }, [productId, isLoggedIn]);
 
   const toggleLike = async () => {
+    if (!isLoggedIn) {
+      Swal.fire({
+        title: "로그인이 필요합니다.",
+        text: "로그인 하시겠습니까?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "확인",
+        cancelButtonText: "취소",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          window.location.href = "/login"; // 로그인 페이지로 이동
+        }
+      });
+      return;
+    }
+
     const newLikedStatus = !liked;
     setLiked(newLikedStatus);
 
