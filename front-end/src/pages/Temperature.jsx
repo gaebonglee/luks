435674c31ff32605e6a9b0fdb6bc93f0d9adbdef
gayoung lee <gost1073@gmail.com>
2024-06-08 12:45:16 +0900

    메인화면업데이트

---
+++ front-end/src/pages/Temperature.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Temperature = () => {
+  return (
+    <div className="Temperature_page">
+      <div className="Temperature_container"></div>
+    </div>
+  );
+};
+
+export default Temperature;
