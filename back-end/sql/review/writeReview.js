9f19bfbcb169a6572f29a2dbd2e8c168e1947fa2
gayoung lee <gost1073@gmail.com>
2024-06-24 12:04:54 +0900

    리뷰오류 해결

--- back-end/sql/review/writeReview.js
+++ back-end/sql/review/writeReview.js
@@ -27,13 +27,30 @@ function getWritableReviews(memberId, callback) {
 
 function getWrittenReviews(memberId, callback) {
   const query = `
-    SELECT r.review_id, r.rating, r.review_text, p.p_image_url, p.p_name, c.color_name, s.size, r.order_id
+    SELECT 
+        r.review_id, 
+        r.rating, 
+        r.review_text, 
+        p.p_image_url, 
+        p.p_name, 
+        c.color_name, 
+        s.size, 
+        r.order_id
     FROM reviews r
     JOIN product p ON r.product_id = p.product_id
-    JOIN order_items oi ON r.product_id = oi.product_id
+    JOIN order_items oi ON r.product_id = oi.product_id AND r.order_id = oi.order_id
     JOIN colors c ON oi.color_id = c.color_id
     JOIN sizes s ON oi.size_id = s.size_id
     WHERE r.member_id = ?
+    GROUP BY 
+        r.review_id, 
+        r.rating, 
+        r.review_text, 
+        p.p_image_url, 
+        p.p_name, 
+        c.color_name, 
+        s.size, 
+        r.order_id;
   `;
 
   connection.query(query, [memberId], (error, results) => {
